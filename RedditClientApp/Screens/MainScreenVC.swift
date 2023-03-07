4ef115d06a289d3ac9980cb9698f2a09b7f5a129
Mustafa Asım Altınışık <mustafa@altinisik.net>
2023-03-07 14:43:03 +0300

    Trending subreddits collection view is now populated with the trending categories coming from Reddit API

--- RedditClientApp/Screens/MainScreenVC.swift
+++ RedditClientApp/Screens/MainScreenVC.swift
@@ -14,21 +14,55 @@ class MainScreenVC: UIViewController {
     @IBOutlet weak var trendingSubredditsCollectionView: UICollectionView!
     @IBOutlet weak var favoriteSubredditsTableView: UITableView!
     
+    var redditAPI = RedditAPI()
+    var topSubreddits: [String: Int] = [:]
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let cellNib = UINib(nibName: "TrendingSubredditsCVC", bundle: nil)
+        trendingSubredditsCollectionView.register(cellNib, forCellWithReuseIdentifier: "trendingSubredditCell")
+        trendingSubredditsCollectionView.dataSource = self
+        trendingSubredditsCollectionView.delegate = self
+        redditAPI.getTopSubredditsFromPopularPosts { [weak self] (subreddits, error) in
+            if let subreddits = subreddits {
+                self?.topSubreddits = subreddits
+                self?.trendingSubredditsCollectionView.reloadData()
+            } else if let error = error {
+                print("Error getting top subreddits: \(error)")
+            }
+        }
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+extension MainScreenVC: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return topSubreddits.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "trendingSubredditCell", for: indexPath) as! TrendingSubredditsCVC
+        let subreddit = Array(topSubreddits.keys)[indexPath.row]
+        cell.trendingSubredditLabel.text = subreddit
+        
+        // Get a random system image
+        let systemImages = [
+            "circle.grid.hex.fill","rectangle.stack.fill","triangle.fill","square.grid.3x1.below.line.grid.1x2.fill","rhombus.fill","hexagon.fill","pentagon.fill","octagon.fill","star.fill","sun.max.fill","moon.fill","cloud.fill","cloud.sun.fill","cloud.rain.fill","cloud.snow.fill","tornado",
+            "hurricane","bolt.fill","umbrella.fill","flame.fill","drop.fill","waveform.path.ecg.rectangle.fill"]
+        let randomIndex = Int.random(in: 0..<systemImages.count)
+        let systemImage = UIImage(systemName: systemImages[randomIndex])
+        cell.trendingSubredditImage.image = systemImage
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewWidth = collectionView.bounds.width
+        let cellWidth = (collectionViewWidth - 20) / 3 // Subtract 20 for spacing between cells
+        let cellHeight = collectionView.bounds.height / 2 // Divide height by 2 to create two rows
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+}
+
