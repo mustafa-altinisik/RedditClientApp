f8971cd66a3068c2b28d3b620755de78abce97ea
Mustafa Asım Altınışık <mustafa@altinisik.net>
2023-03-11 14:41:20 +0300

    Posts Table View Cell is reverted.

---
+++ RedditClientApp/Cells/PostsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostsTableViewCell.swift
+//  RedditClientApp
+//
+//  Created by Asım Altınışık on 17.02.2023.
+//
+
+import UIKit
+
+//This class contains the outlets that are used to display a single post cell on the posts screen.
+class PostsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var postTitle: UILabel!
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var postView: UIView!
+    @IBOutlet weak var postDescription: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
