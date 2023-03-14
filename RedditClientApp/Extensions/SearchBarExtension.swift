82a22358b606d1910e00391367ccd2f2c6da98ea
Mustafa Asım Altınışık <mustafa@altinisik.net>
2023-03-14 10:15:49 +0300

    Comments are added.

--- RedditClientApp/Extensions/SearchBarExtension.swift
+++ RedditClientApp/Extensions/SearchBarExtension.swift
@@ -11,9 +11,11 @@ import UIKit
 // This extension is used to handle the search bar in the MainScreenVC.
 extension MainScreenVC: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // Remove trimming characters.
         guard var subreddit = searchBar.text?.trimmingCharacters(in: .whitespacesAndNewlines), !subreddit.isEmpty else {
             return
         }
+        // Capitalize each word except the first one for camel casing.
         subreddit = subreddit.components(separatedBy: .whitespaces).enumerated().map { (index, word) in
             return index == 0 ? word : word.capitalized
         }.joined()
