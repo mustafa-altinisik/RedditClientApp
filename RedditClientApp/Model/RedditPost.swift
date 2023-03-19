05b5334cc2ba26bb6b8be30b0843e9692e8d0975
Mustafa Asım Altınışık <mustafa@altinisik.net>
2023-03-19 18:53:47 +0300

    Model is modified.

--- RedditClientApp/Model/RedditPost.swift
+++ RedditClientApp/Model/RedditPost.swift
@@ -20,54 +20,32 @@ struct RedditChild: Codable {
 }
 
 struct RedditPostData: Codable {
-    let thumbnail: String
+    let imageURL: String
     let title: String
-    let selftext: String
+    let description: String
     let permalink: String
-    let over_18: Bool
+    let isOver18: Bool
     let subreddit: String?
     let id: String
-
     
     enum CodingKeys: String, CodingKey {
-        case thumbnail
-        case title
-        case selftext
-        case permalink
-        case over_18
-        case subreddit
-        case id
+        case imageURL = "thumbnail"
+        case title = "title"
+        case description = "selftext"
+        case permalink = "permalink"
+        case isOver18 = "over_18"
+        case subreddit = "subreddit"
+        case id = "id"
     }
     
-    // This function is used to convert the data from the API call to RedditPost struct
-    func toRedditPost() -> RedditPost {
-        if let subreddit = subreddit {
-            return RedditPost(imageURL: thumbnail, title: title, description: selftext, permalink: permalink, over_18: over_18, subreddit: subreddit, id: id)
-        } else {
-            return RedditPost(imageURL: thumbnail, title: title, description: selftext, permalink: permalink, over_18: over_18, subreddit: "", id: id)
-        }
-    }
-
-}
-
-
-//This struct is used to store the data of a single post
-struct RedditPost {
-    var imageURL: String
-    var title: String
-    var description: String
-    var permalink: String
-    var over_18: Bool
-    var subreddit: String
-    var id: String
-    
-    init(imageURL: String, title: String, description: String, permalink: String, over_18: Bool, subreddit: String, id: String) {
+    init(imageURL: String, title: String, description: String, permalink: String, isOver18: Bool, subreddit: String?, id: String) {
         self.imageURL = imageURL
         self.title = title
         self.description = description
         self.permalink = permalink
-        self.over_18 = over_18
+        self.isOver18 = isOver18
         self.subreddit = subreddit
         self.id = id
     }
 }
+
