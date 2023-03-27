540702185f47a2512ed53b995626f428e701b507
Mustafa Asım Altınışık <mustafa@altinisik.net>
2023-03-27 14:43:17 +0300

    User default keys are now not hardcoded.

---
+++ RedditClientApp/Supporting Files/UserDefaultKeys.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefaultKeys.swift
+//  RedditClientApp
+//
+//  Created by Asım Altınışık on 25.03.2023.
+//
+
+import Foundation
+
+struct UserDefaultsKeys {
+    static let safeSearch = "safeSearch"
+    static let postsWithImages = "postsWithImages"
+    static let favoriteSubreddits = "favoriteSubreddits"
+}
